2727da38fa38cfd9a6edd204ac06ea91e9fdecb3
Iago Barbosa <iago.barbosa@somapay.com.br>
2025-04-21 17:42:16 -0300

    Finalizando as rotas de transações

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,5 @@
-import fastify from "fastify"
-import { knex } from "./database"
-import { randomUUID } from "node:crypto"
+import { app } from "./app"
 import { env } from "./env"
-
-const app = fastify({
-  logger: true,
-})
-
-app.get("/hello", async () => {
-  // const transactions = await knex("transactions")
-  //   .insert({
-  //     id: randomUUID(),
-  //     title: "New transaction",
-  //     amount: 5000,
-  //   })
-  //   .returning("*")
-  // return transactions
-
-  const transactions = await knex("transactions").select("*")
-  return transactions
-})
 
 app
   .listen({
